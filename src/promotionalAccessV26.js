4fe2c55c50d2c2005786ce2904bd6f9674ae71e8
Executive_Eagle <christophershelley257@gmail.com>
2026-09-04 17:28:12 -0500

    Wire Promotional Access V27 into visible navigation

--- src/promotionalAccessV26.js
+++ src/promotionalAccessV26.js
@@ -1,9 +1,11 @@
 import { startPromotionalAccessV26 } from "./promo/promotionalCoreV26.js";
 import { renderFeaturePage } from "./promo/promotionalFeaturesV26.js";
 import { renderAccessHub, renderPromoAdmin } from "./promo/promotionalAdminV26.js";
+import { startPromotionalNavigationV27 } from "./promo/promotionalNavigationV27.js";
 
 startPromotionalAccessV26({
   renderFeature: renderFeaturePage,
   renderAccessHub,
   renderAdmin: renderPromoAdmin
 });
+startPromotionalNavigationV27();
