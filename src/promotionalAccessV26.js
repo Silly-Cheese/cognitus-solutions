48166ac8f382ef5aa8dc62aed296b7164833c512
Executive_Eagle <christophershelley257@gmail.com>
2026-09-04 17:59:30 -0500

    Start Promotional Access V28 enhancements

--- src/promotionalAccessV26.js
+++ src/promotionalAccessV26.js
@@ -2,6 +2,7 @@ import { startPromotionalAccessV26 } from "./promo/promotionalCoreV26.js";
 import { renderFeaturePage } from "./promo/promotionalFeaturesV26.js";
 import { renderAccessHub, renderPromoAdmin } from "./promo/promotionalAdminV26.js";
 import { startPromotionalNavigationV27 } from "./promo/promotionalNavigationV27.js";
+import { startPromotionalEnhancementsV28 } from "./promo/promotionalEnhancementsV28.js";
 
 startPromotionalAccessV26({
   renderFeature: renderFeaturePage,
@@ -9,3 +10,4 @@ startPromotionalAccessV26({
   renderAdmin: renderPromoAdmin
 });
 startPromotionalNavigationV27();
+startPromotionalEnhancementsV28();
