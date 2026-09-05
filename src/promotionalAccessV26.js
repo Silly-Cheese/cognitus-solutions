7b7a72e26ce5911cfebe19bafb01a4ffa4819ca7
Executive_Eagle <christophershelley257@gmail.com>
2026-09-04 19:04:11 -0500

    Fix promotional feature contrast and add V33 analysis tools
    Adds the V33 contrast contract, Risk Signal Matrix, Organization Overlap Scanner, promo/admin integration, and regression validation.

--- src/promotionalAccessV26.js
+++ src/promotionalAccessV26.js
@@ -1,14 +1,18 @@
 import { startPromotionalAccessV26 } from "./promo/promotionalCoreV26.js";
-import { renderFeaturePage } from "./promo/promotionalFeaturesV26.js";
+import { renderFeaturePageV33 } from "./promo/promotionalFeaturesV33.js";
 import { renderAccessHub, renderPromoAdmin } from "./promo/promotionalAdminV26.js";
 import { startPromotionalNavigationV27 } from "./promo/promotionalNavigationV27.js";
 import { startPromotionalEnhancementsV28 } from "./promo/promotionalEnhancementsV28.js";
 import { startPromotionalMobileV29 } from "./promo/promotionalMobileV29.js";
 import { startPromotionalWorkspacesV30 } from "./promo/promotionalWorkspacesV30.js";
 import { startPromotionalInvestigationsV32 } from "./promo/promotionalInvestigationV32.js";
+import { startPromotionalRegistryV33 } from "./promo/promotionalRegistryV33.js";
+import { startPromotionalContrastV33 } from "./promo/promotionalContrastV33.js";
 
+startPromotionalContrastV33();
+startPromotionalRegistryV33();
 startPromotionalAccessV26({
-  renderFeature: renderFeaturePage,
+  renderFeature: renderFeaturePageV33,
   renderAccessHub,
   renderAdmin: renderPromoAdmin
 });
